a5ead4b9ef2e8c6c39135e44b3744f61128206f2
Sanchjeev <sanchjeev.karikalan@imt-atlantique.net>
2024-11-01 22:19:23 +0100

    Modification de la classe ClearingArc pour ne pas à avoir à redéfinir la méthode step() (on a juste modifié le poids de l'arc 0->1)

--- src/petrinet/ClearingArc.java
+++ src/petrinet/ClearingArc.java
@@ -1,17 +1,13 @@
 package petrinet;
 
 public class ClearingArc extends InArc {
-    // The constructor initializes a ClearingArc with a place and a weight of 0 
+    // The constructor initializes a ClearingArc with a place and a weight of 1, the weight
+	//is set to 1, thus this arc can only step when its place has at least one token
     public ClearingArc(Place place) {
-        super(0, place);
+        super(1, place);
     }
-    
-    // Method to check if the clearing arc can step, which is true if the place has at least one token
-    public boolean canStep() {
-        return place.getToken() > 0;
-    }
-    
     // Method to perform the step action, which removes all tokens from the place
+  
     public void step() {
         place.removeToken(place.getToken());
     }
