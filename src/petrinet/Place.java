d9dd06229fea5e4dc37a84410a551c847a175418
Sanchjeev <sanchjeev.karikalan@imt-atlantique.net>
2024-11-01 22:00:03 +0100

    Modification Place : ajout commentaires en anglais

--- src/petrinet/Place.java
+++ src/petrinet/Place.java
@@ -1,25 +1,31 @@
 package petrinet;
 
 public class Place {
-	public int token;
-	
-	public Place(int token) {
-		this.token = token;
-	}
-	
-	public int getToken() {
-		return token;
-	}
-	
-	public void addToken(int token) {
-		this.token += token;
-	}
-	
-	public void removeToken(int token) {
-		if (this.token >= token) {
-			this.token -= token;
-		} else {
-			System.out.println("Not enough token");
-		}
-	}
+    // The number of tokens currently in this place
+    public int token;
+    
+    // Constructor to initialize the Place with a specified number of tokens
+    public Place(int token) {
+        this.token = token; 
+    }
+    
+    // Method to retrieve the current number of tokens in the place
+    public int getToken() {
+        return token; 
+    }
+    
+    // Method to add tokens to the place
+    public void addToken(int token) {
+        this.token += token; 
+    }
+    
+    // Method to remove a specified number of tokens from the place
+    public void removeToken(int token) {
+        // Check if there are enough tokens to remove
+        if (this.token >= token) {
+            this.token -= token; 
+        } else {
+            System.out.println("Not enough token"); 
+        }
+    }
 }
