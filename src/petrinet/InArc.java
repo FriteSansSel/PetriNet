dddc6291be6bf0cd363c7ef7b7a6d3e5c9dd0868
Sanchjeev <sanchjeev.karikalan@imt-atlantique.net>
2024-11-01 21:56:58 +0100

    Modification de InArc : ajout commentaires en anglais

--- src/petrinet/InArc.java
+++ src/petrinet/InArc.java
@@ -1,19 +1,25 @@
 package petrinet;
 
 public class InArc {
-	public int weight;
-	public Place place;
-	
-	public InArc(int weight, Place place) {
-		this.weight = weight;
-		this.place = place;
-	}
-	
-	public boolean canStep() {
-		return place.getToken() >= weight;
-	}
-	
-	public void step() {
-		place.removeToken(weight);
-	}
+    // The weight of the inArc, representing the number of tokens required
+    public int weight;
+    
+    // The place associated with this InArc
+    public Place place;
+    
+    // Constructor to initialize the InArc with a specified weight and place
+    public InArc(int weight, Place place) {
+        this.weight = weight; 
+        this.place = place;  
+    }
+    
+    // Method to check if the transition can step based on the current token count in the place
+    public boolean canStep() {
+        return place.getToken() >= weight; 
+    }
+    
+    // Method to perform the step action, removing the required tokens from the place
+    public void step() {
+        place.removeToken(weight);
+    }
 }
